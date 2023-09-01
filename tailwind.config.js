1b2e938744c94fe5096a7770288ef3844d7aa846
felicianatania <92023720+felicianatania@users.noreply.github.com>
2023-09-01 21:14:44 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+  ],
+  theme: {
+    extend: {
+        colors:{
+            lightblue: '#CBD6F6',
+            maincolor: '#879EEB',
+            whiteblue: '#EEF1FC',
+            darkblue: '#587AE4',
+            llightblue: '#DDE3F9',
+            darkdarkblue: '#4F69B9',
+        },
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+        fontSize: {
+            headline: '3rem',
+            title: '1.875rem',
+            heading: '1.3125rem',
+            subheading: '1.125rem',
+            name: '1rem',
+            subname: '0.875rem',
+            light: '0.8rem',
+        },
+        borderRadius: {
+            'default': '5px',
+        }
+    },
+  },
+  plugins: [],
+}
+
