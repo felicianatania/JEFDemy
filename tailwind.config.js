f990fc28152769612ed02a32144ca52906f88de7
elyssadg <106426783+elyssadg@users.noreply.github.com>
2023-09-27 16:36:33 +0700

    feats: edit home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,18 @@ export default {
         },
         borderRadius: {
             'default': '5px',
-        }
+        },
+        gradientColorStops: {
+          'blue-pink-yellow': ['#0085FF', '#FF0387', '#FDD92C'],
+          'desain': ['rgba(187, 210, 197, 0.90)', '#536976 '],
+          'pengembangan': ['rgba(249, 212, 35, 0.9)', 'rgba(230, 92, 0, 1) '],
+          'pemasaran': ['rgba(20, 136, 204, 0.9)', 'rgba(43, 50, 178, 1) '],
+          'software': ['rgba(183, 152, 145, 0.90)', '#94716B  '],
+          'pengembangandiri': ['rgba(172, 187, 120, 0.90)', '#799F0C '],
+          'bisnis': ['rgba(123, 67, 151, 0.90)', '#DC2430 '],
+          'kuliner': ['rgba(153, 242, 200, 0.90)', '#1F4037 '],
+          'musik': ['rgba(204, 204, 178, 0.90)', '#757519 ']
+        },
     },
   },
   plugins: [],
